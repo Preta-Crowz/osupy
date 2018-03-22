9213ce0da2f04db3239c5b8ffea51596b0e07397
Preta Crowz <pretacrowz@gmail.com>
2018-03-22 17:54:23 +0900

    [v0.13]

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(name='osupy',
-      version='0.12',
+      version='0.13',
       url='https://github.com/Preta-Crowz/osupy',
       license='MIT',
       author='Preta',
       author_email='preta@crowz.r-e.kr',
       description='osu! Python API',
-      packages=[],
+      packages=['osupy'],
       setup_requires=['requests>=2.13.0'],
       keywords=['osu','osupy']
      )
